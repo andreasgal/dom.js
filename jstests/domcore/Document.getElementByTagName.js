42f54c30f7ffc3b28321a4f93bd17c5d107079de
Tom Austin <tom@bias2build.com>
2011-07-07 12:03:38 -0700

    Updating getElementByTagName test

--- jstests/domcore/Document.getElementByTagName.js
+++ jstests/domcore/Document.getElementByTagName.js
@@ -8,8 +8,10 @@ writeHeaderToLog( SECTION + ": "+ TITLE);
 testdc(function() {
   assert_true(document.getElementsByTagName("html") instanceof NodeList,
       "document.getElementsByTagName('html') instanceof NodeList");
-  assert_false(document.getElementsByTagName("html") instanceof HTMLCollection, "HTMLCollection")
-  assert_true(document.getElementsByTagName("html") !== document.getElementsByTagName("html"), "no caching")
+  assert_false(document.getElementsByTagName("html") instanceof HTMLCollection, "HTMLCollection");
+  assert_true(document.getElementsByTagName("html") !== document.getElementsByTagName("html") ||
+              document.getElementsByTagName("html") === document.getElementsByTagName("html"),
+              "Caching is allowed.");
 })
 testdc(function() {
   var l = document.getElementsByTagName("nosuchtag")
@@ -25,16 +27,20 @@ testdc(function() {
   assert_equals(document.getElementsByTagName("i").length, 0)
   assert_equals(document.body.getElementsByTagName("I").length, 0)
   assert_equals(document.body.getElementsByTagName("i").length, 0)
+  document.body.removeChild(i);
 })
 testdc(function() {
   var t = document.body.appendChild(document.createElementNS("test", "te:st"))
-  assert_equals(document.getElementsByTagName("st").length, 0)
-  assert_equals(document.getElementsByTagName("te:st")[0], t)
+  assert_equals(document.getElementsByTagName("st").length, 1)
+  assert_equals(document.getElementsByTagName("st")[0], t)
+  assert_equals(document.getElementsByTagName("te:st").length, 0)
+  document.body.removeChild(t)
 })
 testdc(function() {
   var h = document.body.appendChild(document.createElementNS("http://www.w3.org/1999/xhtml", "te:st"))
   assert_equals(document.getElementsByTagName("st")[0], h)
   assert_equals(document.getElementsByTagName("TE:ST").length, 0)
+  document.body.removeChild(h);
 })
 
 
