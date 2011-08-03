da741cedbfb4e4338217f5a55f30cbab855bbf06
David Flanagan <dflanagan@mozilla.com>
2011-08-03 16:39:36 -0700

    imported old W3/NIST DOM tests from jsdom project

---
+++ tests/jsdom/buildStaffDocument.js
@@ -0,0 +1,97 @@
+function buildStaffDocument() {
+    var doctype = document.implementation.createDocumentType("staff", "", "");
+    var doc = document.implementation.createDocument("", "", doctype);
+
+    var staff     = doc.createElement("staff");
+    var employees = [];
+    var addresses = [];
+    var names     = [];
+    var positions = [];
+    var genders   = [];
+    var ids       = [];
+    var salaries  = [];
+
+    // create 5 employees
+    for (var i=0; i<5; i++)
+    {
+        var employee = doc.createElement("employee");
+        var address  = doc.createElement("address");
+        var name     = doc.createElement("name");
+        var position = doc.createElement("position");
+        var gender   = doc.createElement("gender");
+        var id       = doc.createElement("employeeId");
+        var salary   = doc.createElement("salary");
+
+        employee.appendChild(id);
+        employee.appendChild(name);
+        employee.appendChild(position);
+        employee.appendChild(salary);
+        employee.appendChild(gender);
+        employee.appendChild(address);
+        staff.appendChild(employee);
+
+        names.push(name);
+        employees.push(employee);
+        addresses.push(address);
+        genders.push(gender);
+        positions.push(position);
+        ids.push(id);
+        salaries.push(salary);
+    }
+
+    ids[0].appendChild(doc.createTextNode("EMP0001"));
+    salaries[0].appendChild(doc.createTextNode("56,000"));
+    addresses[0].setAttribute("domestic", "Yes");
+    addresses[0].appendChild(doc.createTextNode('1230 North Ave. Dallas, Texas 98551'));
+    names[0].appendChild(doc.createTextNode("Margaret Martin"));
+    genders[0].appendChild(doc.createTextNode("Female"));
+    positions[0].appendChild(doc.createTextNode("Accountant"));
+
+
+    ids[1].appendChild(doc.createTextNode("EMP0002"));
+    salaries[1].appendChild(doc.createTextNode("35,000"));
+
+    addresses[1].setAttribute("domestic", "Yes");
+    addresses[1].setAttribute("street", "Yes");
+    addresses[1].appendChild(doc.createTextNode("1900 Dallas Road"));
+    addresses[1].appendChild(doc.createTextNode(" Dallas, "));
+    addresses[1].appendChild(doc.createTextNode("Texas"));
+    addresses[1].appendChild(doc.createTextNode("\n 98554"));
+
+    names[1].appendChild(doc.createTextNode("Martha RaynoldsThis is a CDATASection with EntityReference number 2 1900 Dallas Road\nThis is an adjacent CDATASection with a reference to a tab &tab;"));
+    genders[1].appendChild(doc.createTextNode("Female"));
+    positions[1].appendChild(doc.createTextNode("Secretary"));
+
+    ids[2].appendChild(doc.createTextNode("EMP0003"));
+    salaries[2].appendChild(doc.createTextNode("100,000"));
+    addresses[2].setAttribute("domestic", "Yes");
+    addresses[2].setAttribute("street", "No");
+    addresses[2].appendChild(doc.createTextNode("PO Box 27 Irving, texas 98553"));
+    names[2].appendChild(doc.createTextNode("Roger\n Jones")) ;
+    genders[2].appendChild(doc.createTextNode("&ent4;"));//Text("&ent4"));
+    positions[2].appendChild(doc.createTextNode("Department Manager"));
+
+    ids[3].appendChild(doc.createTextNode("EMP0004"));
+    salaries[3].appendChild(doc.createTextNode("95,000"));
+    addresses[3].setAttribute("domestic", "Yes");
+    addresses[3].setAttribute("street", "Yes");
+    addresses[3].appendChild(doc.createTextNode("27 South Road. Dallas, Texas 98556"));
+    names[3].appendChild(doc.createTextNode("Jeny Oconnor"));
+    genders[3].appendChild(doc.createTextNode("Female"));
+    positions[3].appendChild(doc.createTextNode("Personal Director"));
+
+    ids[4].appendChild(doc.createTextNode("EMP0005"));
+    salaries[4].appendChild(doc.createTextNode("90,000"));
+    addresses[4].setAttribute("street", "Yes");
+    addresses[4].appendChild(doc.createTextNode("1821 Nordic. Road, Irving Texas 98558"));
+    names[4].appendChild(doc.createTextNode("Robert Myers"));
+    genders[4].appendChild(doc.createTextNode("male"));
+    positions[4].appendChild(doc.createTextNode("Computer Specialist"));
+
+    doc.appendChild(doc.createProcessingInstruction("TEST-STYLE", "PIDATA"));
+    doc.appendChild(doc.createComment(" This is comment number 1."));
+    doc.appendChild(staff);
+
+    //doc.normalize();
+    return doc;
+};
---
+++ tests/jsdom/testrunner.js
@@ -0,0 +1,72 @@
+function run(tests) {
+    var t = {
+        pass: 0,
+        fail: 0,
+        total_pass: 0,
+        total_fail: 0,
+
+        equal: function(actual, expected, msg) {
+            if (actual == expected) {
+                this.pass++;
+            }
+            else {
+                this.fail++;
+                print("Assertion failed: " + msg);
+            }
+        },
+
+        notEqual: function(actual, expected, msg) {
+            if (actual != expected) {
+                this.pass++;
+            }
+            else {
+                this.fail++;
+                print("Assertion failed: " + msg);
+            }
+        },
+
+        ok: function(value, msg) {
+            this.equal(true, value, msg);
+        },
+
+        done: function() {
+            if (this.fail > 0) {
+                print("FAIL");
+                fails++;
+            }
+            else {
+                print("PASS");
+                passes++;
+            }
+            this.reset();
+        },
+
+        reset: function() {
+            this.total_pass += this.pass;
+            this.total_fail += this.fail;
+            this.pass = this.fail = 0;
+        }
+
+    };
+
+    var passes = 0, fails = 0, aborts = 0;
+
+    for(var testname in tests) {
+        print("Running " + testname + "...")
+        try {
+            tests[testname](t);
+        }
+        catch(e) {
+            print("Aborted: " + e.message);
+            aborts++;
+            t.reset();
+        }
+    }
+
+    print(t.total_pass + " assertions passed ");
+    print(t.total_fail + " assertions failed ");
+    print(passes + " test cases passed");
+    print(fails + " test cases failed");
+    print(aborts + " test cases aborted");
+    
+}
