783ed86fe78146f11c1a39d78e52b3e684507b9c
Donovan Preston <donovanpreston@gmail.com>
2011-08-16 17:15:15 -0700

    Have test_runner send the disassembly gathered from running the tests to CoverMonkey over stdin. NOTE if you do a git pull on mozilla-central, you will notice the disassembly delimiting lines changed from PC COUNTS to SCRIPT. Make sure you have the latest spidermonkey source.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,3 @@ defineLazyProperty(global, "document", function() {
     return wrap(new impl.DOMImplementation().createHTMLDocument(""),
                idl.Document);
 });
-
-global.assert = assert;
-
