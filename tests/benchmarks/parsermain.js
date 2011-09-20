492586d21562505404f4a48c1a50474fb7f19ead
Donovan Preston <donovanpreston@gmail.com>
2011-09-20 15:57:44 -0700

    Add a tab which dumps dom.js mutation events from the iframe so you can see what is happening when the page is being built/changed.

--- tests/benchmarks/parsermain.js
+++ tests/benchmarks/parsermain.js
@@ -1,6 +1,16 @@
 var originalDocument = document;
-var mode = document.getElementById("mode");
-if (window.location.search === "?domjs") {
+var start = null;
+
+var MUTATE_EVENTS = {
+    1: "MUTATE_VALUE",
+    2: "MUTATE_ATTR",
+    3: "MUTATE_REMOVE_ATTR",
+    4: "MUTATE_REMOVE",
+    5: "MUTATE_MOVE",
+    6: "MUTATE_INSERT"
+}
+
+if (window.location.search === "?domjs" || window.location.search === "?domjsdumpevents") {
     var script = document.createElement("script");
     script.setAttribute("src", "../../dom.js");
     script.setAttribute("type", "application/javascript; version=1.8");
@@ -12,23 +22,66 @@ function submitted(form) {
     return false;
 }
 
-var start = null;
 function cb() {
-    var runtime = originalDocument.createTextNode(Date.now() - start);
+    var url = originalDocument.getElementById("theiframe").src;
+    var date = new Date();
+    var timer = Date.now() - start;
+    var runtime = originalDocument.createTextNode(date + ' ' + url + ' ' + timer);
     var wrapper = originalDocument.createElement('div');
     wrapper.appendChild(runtime);
     originalDocument.getElementById("parsetimes").appendChild(wrapper);
 }
 
+function mutation(o) {
+    var output = originalDocument.getElementById(
+        'outputiframe'
+    ).contentDocument.getElementById(
+        "output"
+    );
+    // type parent index nid child
+    var node = output.ownerDocument.createTextNode(
+        o.nid + ' ' + MUTATE_EVENTS[o.type] + ' ' + o.parent + ' ' + JSON.stringify(o.child));
+    var wrapper = output.ownerDocument.createElement("div");
+    wrapper.appendChild(node);
+    //console.log(output, node, o);
+    try {
+        output.appendChild(wrapper);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 function loaded(foo) {    
     if (window.location.search === "?domjs") {
         originalDocument.getElementById("domjs").className = "selected";
+    } else if (window.location.search === "?domjsdumpevents") {
+        originalDocument.getElementById("domjsdumpevents").className = "selected";
     } else {
         originalDocument.getElementById("native").className = "selected";
     }
 
     var doc = "<html>" + foo.contentDocument.documentElement.innerHTML + "</html>";
     var domjsdoc = document.implementation.createDocument(null, null, null);
+    if (window.location.search === "?domjsdumpevents") {
+        try {
+            domjsdoc.implementation.mozSetOutputMutationHandler(domjsdoc, mutation);
+        } catch (e) {
+            // native dom
+        }
+        var output = originalDocument.getElementById('outputiframe').contentDocument.body;
+        while (output.firstChild) {
+            output.removeChild(output.firstChild);
+        }
+        var wrapper = output.ownerDocument.createElement("div");
+        wrapper.setAttribute("id", "output");
+        output.appendChild(wrapper);
+    } else {
+        var out = originalDocument.getElementById('outputiframe');
+        if (out) {
+            originalDocument.body.removeChild(out);
+        }
+    }
+
     start = Date.now();
     parseHtmlDocument(doc, domjsdoc, cb, null);
 }
