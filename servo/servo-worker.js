ef6e5f4b94f504aad93b28e6a69fb5890c6226f8
Donovan Preston <donovanpreston@gmail.com>
2011-10-31 11:45:14 -0700

    was having problems with xmlhttprequest in a webworker, so move the network io into the main thread

--- servo/servo-worker.js
+++ servo/servo-worker.js
@@ -5,14 +5,7 @@ function mutation(evt) {
     postMessage(JSON.stringify(evt));
 }
 
-function GET(url) {
-    var req = new XMLHttpRequest();
-    req.open("GET", url, false); // in a thread, so blocking ok
-    req.send('');
-    var newdoc = document.implementation.mozHTMLParser(mutation).end(this.responseText);
-}
-
 onmessage = function(message) {
-    var url = message.data.url.toString();
-    GET(url);
+    var body = message.data.body.toString();
+    var newdoc = document.implementation.mozHTMLParser(mutation).end(body);
 }
