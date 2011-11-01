5033d20e26a8f7cac0890fe157f388333683be61
Donovan Preston <donovanpreston@gmail.com>
2011-11-01 00:05:54 -0700

    Add the ability to click a node id to collapse or expand all it's children

--- servo/servo-worker.js
+++ servo/servo-worker.js
@@ -9,4 +9,5 @@ onmessage = function(message) {
     var body = message.data.body.toString();
     var url = message.data.url.toString();
     var newdoc = document.implementation.mozHTMLParser(url, mutation).end(body);
+    postMessage(JSON.stringify("complete"));
 }
