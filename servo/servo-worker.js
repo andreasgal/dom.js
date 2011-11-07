7b1f91cb2cb842792519bf5226b114bbc048070f
Donovan Preston <donovanpreston@gmail.com>
2011-11-07 15:03:19 -0800

    Update to use the latest parser apis.

--- servo/servo-worker.js
+++ servo/servo-worker.js
@@ -6,8 +6,15 @@ function mutation(evt) {
 }
 
 onmessage = function(message) {
+	try {
     var body = message.data.body.toString();
     var url = message.data.url.toString();
-    var newdoc = document.implementation.mozHTMLParser(url, mutation).end(body);
+    var parser = document.implementation.mozHTMLParser(url)
+	var doc = parser.document();
+	doc.implementation.mozSetOutputMutationHandler(doc, mutation);
+	parser.parse(body, true);
     postMessage(JSON.stringify("complete"));
+	} catch (e) {
+		postMessage(JSON.stringify(e.toString()));
+	}
 }
